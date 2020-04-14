0cd87b45cf8af4cb3db0182422d3a786c4fa0335
virdzek <virdzek@avast.com>
2020-04-14 19:23:19 +0200

    + Added skelet of the app

---
+++ DailyQuotes/TableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  TableViewController.swift
+//  DailyQuotes
+//
+//  Created by Pavol Virdzek on 08/04/2020.
+//  Copyright © 2020 Pavol Virdzek. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    let notifications = ["Local Notification",
+                         "Local Notification with Action",
+                         "Local Notification with Content",
+                         "Push Notification with  APNs",
+                         "Push Notification with Firebase",
+                         "Push Notification with Content"]
+    
+    var appDelegate = UIApplication.shared.delegate as? AppDelegate
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return notifications.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = notifications[indexPath.row]
+
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let notificationType = notifications[indexPath.row]
+        
+        let alert = UIAlertController(title: "",
+                                      message: "After 5 seconds " + notificationType + " will appear",
+                                      preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            
+            self.appDelegate?.scheduleNotification(notificationType: notificationType)
+        }
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+
+}
